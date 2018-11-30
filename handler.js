8e4ed5ee82b756089d41870b5c714fae17cc8e11
Henry Han <hanhenry88@gmail.com>
2018-11-30 18:38:21 +0800

    add date handler

--- handler.js
+++ handler.js
@@ -1,14 +1,25 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getDate = async (event, context) => {
+  const newDate = new Date();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: newDate,
+      input: event
+    })
+  };
 };
