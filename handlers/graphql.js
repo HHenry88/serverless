53c7f887fe06c7a655558b72a47b8d0f4361a0e2
Henry Han <hanhenry88@gmail.com>
2018-12-15 16:58:04 +0800

    Add new schema

--- handlers/graphql.js
+++ handlers/graphql.js
@@ -7,3 +7,25 @@ const {
   GraphQLString,
   GraphQLNonNull
 } = require("graphql");
+
+const myGreeting = fN => `Hello, ${fN}. We're using Graphql`;
+
+const newSchema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "AnyName",
+    fields: {
+      greeting: {
+        args: {
+          firstName: {
+            name: "firstName",
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        type: GraphQLString,
+        resolve: (parent, args) => {
+          return myGreeting(args.firstName);
+        }
+      }
+    }
+  })
+});
