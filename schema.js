20ef665a4792f1ea7ec044bbf08ba2a2a51bdcdf
Henry Han <hanhenry88@gmail.com>
2018-12-15 16:41:31 +0800

    Remove apollo. Use native graphql

---
+++ schema.js
@@ -0,0 +1,31 @@
+const schema = `
+  type Query {
+    getUserInfo(handle: String!): User!
+  }
+
+  type Tweet {
+    tweet_id: String!
+    tweet: String!
+    handle: String!
+    created_at: String!
+  }
+
+  type TweetConnection {
+    items: [Tweet!]!
+    nextToken: String
+  }
+
+  type User {
+    name: String!
+    description: String!
+    followers_count: Int!
+    following: [String!]!
+    topTweet: Tweet
+    tweets(limit: Int!, nextToken: String): TweetConnection
+  }
+`;
+
+export { schema };
+
+// eslint-disable-next-line import/prefer-default-export
+export { schema };
